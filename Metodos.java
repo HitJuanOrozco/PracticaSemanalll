ecd0a27296d29052248c11339e2848c7a4911164
HitJuanOrozco <juancamiloorozcob@gmail.com>
2025-02-20 20:22:20 -0500

    Actualizacion 20022025

---
+++ Metodos.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Metodos {
+    Scanner sc = new Scanner(System.in);
+
+    public Producto[][] llenarMatriz(int dimension) {
+        Producto[][] m = new Producto[dimension][dimension];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                Producto o = new Producto();
+                System.out.println("Ingrese el nombre del producto");
+                o.setNombre(sc.next());
+                System.out.println("Ingrese el precio del producto");
+                o.setPrecio(sc.nextDouble());
+                System.out.println("ingrese la cantidad del producto");
+                o.setCantidad(sc.nextInt());
+                m[i][j] = o;
+            }
+        }
+        return m;
+
+    }
+
+    public void mostrarMatriz(Producto[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                System.out.println("Nombre: " + m[i][j].getNombre());
+                System.out.println("Precio: " + m[i][j].getPrecio());
+                System.out.println("Cantidad: " + m[i][j].getCantidad());
+            }
+            System.out.println();
+        }
+    }
+
+    public String MostrarposicionElemento(Producto[][] m) {
+        String nombreBuscar = "";
+        System.out.println("Ingrese el nombre del prooducto a buscar");
+        nombreBuscar = sc.next();
+        String result = "";
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                if (m[i][j].getNombre().toLowerCase().equals(nombreBuscar)) {
+                    result += "el producto se encuentra en la posicion: " + (i + 1) + "," + (j + 1
+                    ) + "\n";
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ Punto1.java
@@ -0,0 +1,9 @@
+public class Punto1 {
+    public void P1(int d) {
+        Metodos m = new Metodos();
+        Producto[][] M = new Producto[d][d];
+        M = m.llenarMatriz(d);
+        m.mostrarMatriz(M);
+        System.out.println(m.MostrarposicionElemento(M));
+    }
+}
